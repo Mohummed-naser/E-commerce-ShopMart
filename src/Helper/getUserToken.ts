72d277c1ccfc546a246d0bf7a1b6b55dc7652a97
Mohummed-naser <$ git config --global user.email YOUR_EMAIL>
2025-12-18 21:37:15 +0200

    mohamed naser

--- src/Helper/getUserToken.ts
+++ src/Helper/getUserToken.ts
@@ -1,11 +1,11 @@
-import { decode } from "next-auth/jwt";
-import { cookies } from "next/headers";
+// import { decode } from "next-auth/jwt";
+// import { cookies } from "next/headers";
 
-export async function getUserToken() {
-  const userToken = (await cookies()).get("next-auth.session-token")?.value;
-  const accessToken = await decode({
-    token: userToken,
-    secret: process.env.NEXTAUTH_SECRET!,
-  });
-    return accessToken?.token
-}
+// export async function getUserToken() {
+//   const userToken = (await cookies()).get("next-auth.session-token")?.value;
+//   const accessToken = await decode({
+//     token: userToken,
+//     secret: process.env.NEXTAUTH_SECRET!,
+//   });
+//     return accessToken?.token
+// }
