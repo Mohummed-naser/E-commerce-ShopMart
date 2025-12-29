0ec3348c0ab4db64f83e49579c5bb893dbb8ea88
Mohummed-naser <$ git config --global user.email YOUR_EMAIL>
2025-12-29 21:38:08 +0200

    token update

--- src/Helper/getUserToken.ts
+++ src/Helper/getUserToken.ts
@@ -2,7 +2,10 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const userToken = (await cookies()).get("next-auth.session-token")?.value;
+  const mycookies = await cookies()
+    const userToken =
+      mycookies.get("next-auth.session-token")?.value ||
+      mycookies.get("__secure-next-auth.session-token")?.value;
   const accessToken = await decode({
     token: userToken,
     secret: process.env.NEXTAUTH_SECRET!,
